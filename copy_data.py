d9f9b66e67d746d6bb133746437eb9fbf8dea8fc
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-03-30 15:19:17 +0200

    Copy geojson files also

--- copy_data.py
+++ copy_data.py
@@ -13,11 +13,15 @@ dest_folder = r"D:\SVILUPPO\COVID19-Abruzzo"
 print("Copia i CSV delle Regioni")
 copyfile(os.path.join(orig_folder, r'csv/dpc_regioni_full.csv'), os.path.join(dest_folder,  r'dati-regioni/dpc_regioni_full.csv'))
 copyfile(os.path.join(orig_folder, r'csv/dpc_regioni_latest.csv'), os.path.join(dest_folder,  r'dati-regioni/dpc_regioni_latest.csv'))
+print("Copia il geojson delle Regioni")
+copyfile(os.path.join(orig_folder, r'geo/dpc_regioni_latest.geojson'), os.path.join(dest_folder,  r'dati-regioni/dpc_regioni_latest.geojson'))
 
 # Copia csv province
 print("Copia i CSV delle Province")
 copyfile(os.path.join(orig_folder, r'csv/dpc_province_full.csv'), os.path.join(dest_folder,  r'dati-province/dpc_province_full.csv'))
 copyfile(os.path.join(orig_folder, r'csv/dpc_province_latest.csv'), os.path.join(dest_folder,  r'dati-province/dpc_province_latest.csv'))
+print("Copia il geojson delle Province")
+copyfile(os.path.join(orig_folder, r'geo/dpc_province_latest.geojson'), os.path.join(dest_folder,  r'dati-regioni/dpc_province_latest.geojson'))
 
 # Copia report
 print("Copia Report")
