a19bc70b825e9e48d2bcb5384572e05db5d0f173
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-04-21 18:58:57 +0200

    IZS data added

--- copy_data.py
+++ copy_data.py
@@ -23,6 +23,8 @@ copyfile(os.path.join(orig_folder, r'csv/dpc_province_latest.csv'), os.path.join
 print("Copia il geojson delle Province")
 copyfile(os.path.join(orig_folder, r'geo/dpc_province_latest.geojson'), os.path.join(dest_folder,  r'dati-province/dpc_province_latest.geojson'))
 
-# Copia report
-# print("Copia Report")
-# copyfile(os.path.join(orig_folder, r'report/report.pdf'), os.path.join(dest_folder,  r'report/report.pdf'))
+# Copia dati izs
+print("Copia Report")
+copyfile(os.path.join(orig_folder, r'report/report.pdf'), os.path.join(dest_folder,  r'report/report.pdf'))
+print("Copia dati analisi IZSAM")
+copyfile(os.path.join(orig_folder, r'izs_dati/COVID_IZSAM.csv'), os.path.join(dest_folder,  r'izs_dati/COVID_IZSAM.csv'))
