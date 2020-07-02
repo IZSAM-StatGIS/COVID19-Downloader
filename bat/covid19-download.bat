320d724a82d08a350eb4c0ad3680f167487c4bfb
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-07-02 15:50:30 +0200

    Download procedure transfered to server

--- bat/covid19-download.bat
+++ bat/covid19-download.bat
@@ -1,9 +1,9 @@
-set root=C:\Users\a.dilorenzo\AppData\Local\Continuum\anaconda3
+set root=C:\Users\aa.dilorenzo\AppData\Local\Continuum\anaconda3
 
 call %root%\Scripts\activate.bat %root%
 
 call conda activate geocovid19
 
-call python D:\SVILUPPO\COVID19-Downloader\download_data.py
+call python D:\COVID19\COVID19-Downloader\download_data.py
 
 :: pause
